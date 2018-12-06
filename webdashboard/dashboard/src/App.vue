676adeb54293405df3a9826e2c47a96eed72d8e3
Campbell Vertesi <campbell@vertesi.com>
2018-12-06 14:29:47 +0300

    dummy data in web dashboard

--- webdashboard/dashboard/src/App.vue
+++ webdashboard/dashboard/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ msg }}</h1>
+    <minecraft></minecraft>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import minecraft from './components/MinecraftServers.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    minecraft
+  },
+  data() {
+    return {
+      msg: 'Best Minecraft servers EVAR'
+    }
   }
 }
 </script>
---
+++ webdashboard/dashboard/src/components/MinecraftServers.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="servers">
+    <table>
+      <thead>
+        <tr>
+          <th>Server name</th>
+          <th>Minecraft address</th>
+          <th>RCon address</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(server, index) in servers"
+          :key="index"
+          >
+            <td>{{ server.name }}</td>
+            <td>{{ server.endpoints.minecraft }}</td>
+            <td>{{ server.endpoints.rcon }}</td>
+            </tr>
+      </tbody>
+     </table>
+  </div>
+</template>
+
+<script>
+//import store from '../store';
+export default {
+	data() {
+    return {
+      servers: [
+        {
+          name: 'Dummy server',
+          endpoints: {
+            minecraft: '10.0.0.1:25565',
+            rcon: '10.0.0.1:25575'
+          }
+        },
+        {
+          name: 'Another server',
+          endpoints: {
+            minecraft: '10.0.0.2:25565',
+            rcon: '10.0.0.2:25575'
+          }
+        }
+      ]
+    }
+	//computed: { servers: () => store.state.servers },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
