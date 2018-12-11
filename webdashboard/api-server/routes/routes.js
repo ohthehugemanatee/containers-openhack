c7fce0241d215e3f6f16691136907c94bad6c736
Campbell Vertesi <campbell@vertesi.com>
2018-12-11 23:22:39 +0100

    Get endpoints from k8s api

--- webdashboard/api-server/routes/routes.js
+++ webdashboard/api-server/routes/routes.js
@@ -6,6 +6,7 @@ var appRouter = function(app) {
   });
 
   app.get("/servers", function(req, res) {
+    var servers = getServers();
     const serversMock = [
       {
         name: 'Dummy server',
@@ -25,6 +26,16 @@ var appRouter = function(app) {
     return res.send(serversMock);
 });
 
+}
+
+async function getServers() {
+  const path = '/home/ohthehugemanatee/.kube/config'
+  const Client = require('kubernetes-client').Client
+  const config = require('kubernetes-client').config
+  const client = new Client({ config: config.fromKubeconfig(path), version: '1.10' })
+  const endpoints = await client.api.v1.namespaces('default').endpoints.get()
+//  const namespaces = await client.api.v1.namespaces.get()
+  console.dir(endpoints.body.items);
 }
 
 module.exports = appRouter;
