dc56e27b58b897627ad1e6a7eee74eaa60160df5
Campbell Vertesi <campbell@vertesi.com>
2018-12-14 14:07:28 +0100

    Get Load balancers and list their IPs

--- webdashboard/api-server/routes/routes.js
+++ webdashboard/api-server/routes/routes.js
@@ -1,29 +1,13 @@
-var appRouter = function(app) {
+var appRouter = async function(app) {
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
-  app.get("/servers", function(req, res) {
-    var servers = getServers();
-    const serversMock = [
-      {
-        name: 'Dummy server',
-        endpoints: {
-          minecraft: '10.0.0.1:25565',
-          rcon: '10.0.0.1:25575'
-        }
-      },
-      {
-        name: 'Another server',
-        endpoints: {
-          minecraft: '10.0.0.2:25565',
-          rcon: '10.0.0.2:25575'
-        }
-      }
-    ];
-    return res.send(serversMock);
+  app.get("/servers", async function(req, res) {
+    var servers = await getServers();
+    return res.send(servers);
 });
 
 }
@@ -33,9 +17,19 @@ async function getServers() {
   const Client = require('kubernetes-client').Client
   const config = require('kubernetes-client').config
   const client = new Client({ config: config.fromKubeconfig(path), version: '1.10' })
-  const endpoints = await client.api.v1.namespaces('default').endpoints.get()
-//  const namespaces = await client.api.v1.namespaces.get()
-  console.dir(endpoints.body.items);
+  const services = await client.api.v1.namespaces('default').services.get()
+  var response = services.body.items.map(service => {
+    if (service.spec.type === 'LoadBalancer') {
+      return {
+        name: 'Dummy name',
+        endpoints: {
+          minecraft: service.status.loadBalancer.ingress[0].ip + ':25565',
+          rcon: service.status.loadBalancer.ingress[0].ip + ':25575'
+        }
+      }
+    }
+  }).filter(Boolean);
+  return response;
 }
 
 module.exports = appRouter;
